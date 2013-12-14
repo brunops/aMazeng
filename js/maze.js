336a27999956238afcb2cd407c7797a8959b973f
Bruno Sanches <brunopsanches@gmail.com>
2013-12-13 23:10:32 -0800

    DFS maze generator first try

--- js/maze.js
+++ js/maze.js
@@ -1,3 +1,12 @@
+var _ = require('underscore');
+
+var WALLS = {
+  0: 'TOP',
+  1: 'RIGHT',
+  2: 'BOTTOM',
+  3: 'LEFT'
+};
+
 function Cell() {
   this.init();
 }
@@ -5,10 +14,12 @@ function Cell() {
 Cell.prototype.init = function() {
   this.explored = false;
 
-  this.leftWall    = 1;
-  this.righttWall  = 1;
-  this.topWall     = 1;
-  this.bottomtWall = 1;
+  this.walls = {
+    'TOP'   : true,
+    'RIGHT' : true,
+    'BOTTOM': true,
+    'LEFT'  : true
+  };
 };
 
 // ----------------------------------------------------
@@ -25,4 +36,93 @@ Maze.prototype.init = function(size) {
 Maze.prototype.generateMaze = function() {
   this.maze = [];
 
+  for (var i = 0; i < this.size; ++i) {
+    this.maze.push([]);
+    for (var j = 0; j < this.size; j++) {
+      this.maze[i].push(new Cell());
+    }
+  }
+
+  this.dfs();
 };
+
+Maze.prototype.dfs = function(row, col) {
+  if (typeof row === 'undefined' || typeof col === 'undefined') {
+    row = Math.floor(Math.random() * this.size);
+    col = Math.floor(Math.random() * this.size);
+  }
+
+  var currentCell = this.maze[row][col];
+  currentCell.explored = true;
+
+  var orderToExplore = this.getRandomOrder();
+
+  while (orderToExplore.length) {
+    var nextCell;
+
+    switch (WALLS[orderToExplore.pop()]) {
+      case 'TOP':
+        if (row - 1 < 0) break;
+
+        nextCell = this.maze[row - 1][col];
+
+        // remove walls between cells
+        if (!nextCell.explored) {
+          currentCell['TOP'] = false;
+          nextCell.walls['BOTTOM'] = false;
+
+          this.dfs(row - 1, col);
+        }
+        break;
+
+      case 'RIGHT':
+        if (col + 1 >= this.size) break;
+
+        nextCell = this.maze[row][col + 1];
+
+        // remove walls between cells
+        if (!nextCell.explored) {
+          currentCell['RIGHT'] = false;
+          nextCell.walls['LEFT'] = false;
+
+          this.dfs(row, col + 1);
+        }
+        break;
+
+      case 'BOTTOM':
+        if (row + 1 >= this.size) break;
+
+        nextCell = this.maze[row + 1][col];
+
+        // remove walls between cells
+        if (!nextCell.explored) {
+          currentCell['BOTTOM'] = false;
+          nextCell.walls['TOP'] = false;
+
+          this.dfs(row + 1, col);
+        }
+        break;
+
+      case 'LEFT':
+        if (col - 1 < 0) break;
+
+        nextCell = this.maze[row][col - 1];
+
+        // remove walls between cells
+        if (!nextCell.explored) {
+          currentCell['LEFT'] = false;
+          nextCell.walls['RIGHT'] = false;
+
+          this.dfs(row, col - 1);
+        }
+        break;
+    }
+  }
+};
+
+Maze.prototype.getRandomOrder = function() {
+  return _.shuffle([0, 1, 2, 3]);
+};
+
+
+
