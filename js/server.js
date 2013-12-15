a2612a334b834ebcc58a415bef0dd642e72d89b4
Bruno Sanches <brunopsanches@gmail.com>
2013-12-14 18:02:35 -0800

    Serve application from server
    * serve files using 'send'
    * paint player and enemy with different colors
    * create server shell file

--- js/server.js
+++ js/server.js
@@ -1,7 +1,17 @@
-var io = require('socket.io').listen(3000);
+var sio = require('socket.io');
+var http = require('http');
+var send = require('send');
 
 var games = [];
 var nextRoom = [];
+
+
+var server = http.createServer(function(req, res) {
+  send(req, req.url).root(__dirname + '/..').pipe(res);
+});
+
+server.listen(3000);
+var io = sio.listen(server);
 
 io.sockets.on('connection', function(socket) {
   var gameIndex = games.length;
